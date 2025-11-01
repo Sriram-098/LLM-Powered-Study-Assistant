f87382c77bbfb7ef1e399a3412f9fbc96e6a19a4
Sriram-098 <sriramsenapathi842@gmail.com>
2025-11-01 23:26:02 +0530

    chore: Add common index module for components ease of import

---
+++ client/src/components/index.js
@@ -0,0 +1,28 @@
+// Common Components
+export { default as LoadingSpinner } from './LoadingSpinner'
+export { default as PageHeader } from './PageHeader'
+export { default as EmptyState } from './EmptyState'
+export { default as Button } from './Button'
+export { default as Card } from './Card'
+export { default as FileUpload } from './FileUpload'
+export { default as InputTypeSelector } from './InputTypeSelector'
+export { default as TextInput } from './TextInput'
+
+// Auth Components
+export { default as AuthCard } from './AuthCard'
+export { default as FormInput } from './FormInput'
+export { default as LoginForm } from './LoginForm'
+export { default as RegisterForm } from './RegisterForm'
+
+// Dashboard Components
+export { default as WelcomeSection } from './WelcomeSection'
+export { default as StatsCard } from './StatsCard'
+export { default as QuickActions } from './QuickActions'
+export { default as RecentUploads } from './RecentUploads'
+
+// Layout Components
+export { default as Header } from './Header'
+export { default as QuizTaker } from './QuizTaker'
+export { default as MarkdownRenderer } from './MarkdownRenderer'
+export { default as ConnectionTest } from './ConnectionTest'
+export { default as AIProcessingAnimation } from './AIProcessingAnimation'
